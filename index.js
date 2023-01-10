2043b14b79567ff2c1b98fbdffea450e4a30904f
bbutter98 <bmaselko98@gmail.com>
2023-01-10 02:05:05 +0100

    it's final version, i tried my best to make it good and functional

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-const player = (name, sign) => {
-    wins = 0;
-    draws = 0;
-    loses = 0;
-    const getInfo = () =>{
-        return `Player ${this.name} wins ${this.wins} times, loses ${this.loses} times and make draw ${this.draws} times.`;
+class player{
+    constructor(name, sign){
+        this.name = name;
+        this.sign = sign;
+        this.wins = 0;
+        this.loses = 0;
+        this.winstreak = 0;
+        this.losestreak = 0;
     }
-    const getSign = () =>{
-        return this.sign;
+    getInfo = () =>{
+        return `
+            <h1>${this.name}</h1>
+            <p>Sign: ${this.sign}</p>
+            <p>wins: ${this.wins}</p>
+            <p>loses: ${this.loses}</p>
+            <p>Winstreak: ${this.winstreak}</p>
+            <p>Losestreak: ${this.losestreak}</p>
+         `;
     }
-    return {name,getInfo,getSign}
+    setStreak = (setter) =>{
+        if(setter=="win"){
+            this.winstreak++;
+            this.losestreak = 0;
+        }
+        else if(setter=="lose"){
+            this.losestreak++;
+            this.winstreak = 0;
+        }
+        else{
+            this.winstreak = 0;
+            this.losestreak = 0;
+        }
+    }
+    winCounter = () =>{
+        this.wins++;
+    }
+    loseCounter = () =>{
+        this.loses++;
+    }
+   
 }
+const reset = document.querySelector("button#reset");
+const start = document.querySelector("button#start");
+const p1 = document.querySelector("section#p1");
+const p2 = document.querySelector("section#p2");
+const player1 = new player("player1","X");
+const player2 = new player("player2","O");
 function generateBlankBoard(){
     const boardContainer = document.querySelector("section#board");
         for(let i=0;i<9;i++){
@@ -47,7 +82,6 @@ function checkCombos(array, sign){
     return false;
 }
 function checkWin(array1, array2){
-    //console.log("check win every time array: "+array1+" "+array2);
     let message = "";
     if(checkCombos(array1,"X")==true){
         message = "p1win";
@@ -60,19 +94,24 @@ function checkWin(array1, array2){
     }
     return message;
 }
+function updateStatus(){
+    p1.innerHTML = "";
+    p1.innerHTML = player1.getInfo();
+    p2.innerHTML = "";
+    p2.innerHTML = player2.getInfo();
+}
 function startGame(){
+    document.querySelector("section#tellwinner").textContent = "GOOD LUCK!";
     let isX = true;
-    console.log(isX);
     let signMark = "";
     var iterator = 0;
     var arrayX = [];
     var arrayO = [];
-    player1 = player("player1","X");
-    player2 = player("player2","O");
+    var winner = false;
     const divs = document.querySelectorAll("div.square");
     divs.forEach(element => {
         element.addEventListener("click",function(e){
-            if(element.textContent == ""){
+            if(element.textContent == "" && winner==false){
                 console.log(isX);
                 let status;
                 if(isX){
@@ -87,19 +126,35 @@ function startGame(){
                 }
                 element.textContent = signMark;
                 let winCheck = checkWin(arrayX,arrayO);
+                const tellWinner = document.querySelector("section#tellwinner");
                 if(winCheck === "draw" && iterator == 8){
-                    alert("draw");
+                    winner = true;
                     status=true;
+                    tellWinner.textContent = "It's draw, nobody loses, still good right?";
+                    player1.setStreak("draw");
+                    player2.setStreak("draw");
                 }
                 else if(winCheck == "p1win"){
-                    alert("player 1 won");
+                    winner = true;
                     status=true;
+                    tellWinner.textContent = `${player1.name} is the winner`;
+                    player1.winCounter();
+                    player1.setStreak("win");
+                    player2.loseCounter();
+                    player2.setStreak("lose");
+                    
                 }
                 else if(winCheck == "p2win"){
-                    alert("player 2 won");
+                    winner = true;
                     status=true;
+                    tellWinner.textContent = `${player2.name} is the winner`;
+                    player2.winCounter();
+                    player2.setStreak("win");
+                    player1.loseCounter();
+                    player1.setStreak("lose");
                 }
                 iterator++;
+                updateStatus();
                 if(status){
                     arrayX.length = 0;
                     arrayO.length = 0;
@@ -112,6 +167,10 @@ function startGame(){
         });
     });
 }
+function startPlaying(){
+    startGame();
+    start.removeEventListener("click",startPlaying);
+}
 function resetGame(){
     const divs = document.querySelectorAll("div.square");
     divs.forEach(element => {
@@ -119,10 +178,14 @@ function resetGame(){
     });
     startGame();
 }
-
-
-const reset = document.querySelector("button#reset");
-const start = document.querySelector("button#start");
-start.addEventListener("click",startGame);
+function generateStartInfo(){
+    p1.innerHTML = player1.getInfo();
+    p2.innerHTML = player2.getInfo();
+}
+function loadSettings(){
+    generateBlankBoard();
+    generateStartInfo();
+}
+start.addEventListener("click",startPlaying);
 reset.addEventListener("click",resetGame);
-window.addEventListener("load",generateBlankBoard);
+window.addEventListener("load",loadSettings);
